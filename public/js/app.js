57e0d4fe7c0a240fa25b5cd001fe9b36a9ae0c08
Krunal Patel <kppatel727@gmail.com>
2020-05-05 14:24:01 -0700

    added voting feature

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,91 @@
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    this.state = { products: [] };
+  }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
   render() {
+    const products = this.state.products.sort((a, b) => b.votes - a.votes);
+    const productComponents = products.map(product => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
+      />
+    ));
+
     return (
       <div className='ui unstackable items'>
-        <Product />
+        {productComponents}
       </div>
     );
+  }
+
+  handleProductUpVote(productId) {
+    const nextProducts = this.state.products.map(product => {
+      if (product.id === productId) {
+        return Object.assign({}, product, { votes: product.votes + 1 });
+      } else {
+        return product;
+      }
+    });
+    this.setState({products: nextProducts});
   }
 }
 
 class Product extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
   render() {
     return (
       <div className='item'>
         <div className='image'>
-          <img src='images/products/image-aqua.png' />
+          <img src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
+          <div className='header'>
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
           <div className='description'>
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two week\s.</p>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
-            <img className='ui avatar image' src='images/avatars/daniel.jpg' />
+            <img
+              className='ui avatar image'
+              src={this.props.submitterAvatarUrl}
+            />
           </div>
         </div>
       </div>
     );
+  }
+
+  handleUpVote() {
+    this.props.onVote(this.props.id);
   }
 }
 
